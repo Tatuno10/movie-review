d1a6bc00c055c0d619bb0993fe6caf5b7ba8351c
Tatuno10 <zell8130@outlook.jp>
2021-01-10 05:37:58 +0900

    Update 20210109203400_create_movies.rb

--- db/migrate/20210109203400_create_movies.rb
+++ db/migrate/20210109203400_create_movies.rb
@@ -1,8 +1,11 @@
 class CreateMovies < ActiveRecord::Migration[6.0]
   def change
     create_table :movies do |t|
-
-      t.timestamps
+      t.string :title
+      t.string :titleruby
+      t.image :string
+      t.text :synopsis
+      t.timestamps 
     end
   end
 end
