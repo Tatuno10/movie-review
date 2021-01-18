361f14f63da1eca6833f1203f6df9bd1dab31bf7
Tatuno10 <zell8130@outlook.jp>
2021-01-18 11:29:36 +0900

    add before action index function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,7 @@
 class MoviesController < ApplicationController
-before_action:
+before_action :movies, only: [:index, :show]
 
   def index
-    @movies = Movie.all
-    @moviesup = Movie.order("id DESC")
   end
 
   def new
@@ -18,14 +16,18 @@ before_action:
   end
 
   def show
-    @movies = Movie.all
-    @moviesup = Movie.order("id DESC")
     @movie = Movie.find(params[:id])
     @reviews = Review.all
     @review = Review.new
   end
+  
 
   private
+
+  def movies
+    @movies = Movie.all
+    @moviesup = Movie.order("id DESC")
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :titleruby, :synopsis, :image)
