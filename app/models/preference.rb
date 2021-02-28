6e555f7ba554b56f4abd02931d820ce960a3826f
Tatuno10 <zell8130@outlook.jp>
2021-02-28 15:53:15 +0900

    Update preference.rb

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,7 +1,7 @@
 class Preference < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :genre, class_name: 'genre1', optional: true
-  belongs_to_active_hash :genre, class_name: 'genre2', optional: true
+  belongs_to_active_hash :genre1, class_name: 'Genre', optional: true
+  belongs_to_active_hash :genre2, class_name: 'Genre', optional: true
   belongs_to :tag, class_name:'category1', optional: true
   belongs_to :tag, class_name:'category2', optional: true
   belongs_to :user, optional: true
