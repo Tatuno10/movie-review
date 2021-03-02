7f5c1ac39672d009899472171d0a53c18df32eb9
Tatuno10 <zell8130@outlook.jp>
2021-03-03 00:47:39 +0900

    Fix the login screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :movies
+  def show
+  end
+  private
+  def movies
+    @tag = Tag.all
+    @movies = Movie.all
+    @moviesup = Movie.order("id DESC")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
     end
   end
   resources :tags, only: [:index, :new, :create]
+  resources :users 
 end
