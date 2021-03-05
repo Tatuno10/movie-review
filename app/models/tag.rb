d3eab42b5326f9a11e623f3dcc10a1223d06cad9
Tatuno10 <zell8130@outlook.jp>
2021-03-05 19:02:35 +0900

    Add-validates

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,6 @@ class Tag < ApplicationRecord
   has_many :movie_tags
   has_many :movies, through: :movie_tags
   has_many :preferences
+
+  validates :name, presence: {message: 'は空で入力しないでください。'}
 end
