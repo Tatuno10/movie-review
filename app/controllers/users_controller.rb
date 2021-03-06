50a250276a052538edcc1c2c20faa8032bd6c368
Tatuno10 <zell8130@outlook.jp>
2021-03-06 15:11:34 +0900

    Change-current_user-to-@user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,39 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :movies
+
   def show
     @reviews = Review.where(user_id: current_user.id)
   end
+  
+  def edit
+    @preference = Preference.find(params[:id])
+  end
+
+  def update
+    if @user.preference.update(preference_params)
+      redirect_to user_path(current_user.id), notice: "ジャンル、カテゴリーを編集しました"
+    else
+      flash.now[:alert] = @preference.errors.full_messages
+      render :edit
+    end
+  end
+
   private
   def movies
+    @user = current_user
     @q = Movie.ransack
     @tag = Tag.all
     @movies = Movie.all
     @moviesup = Movie.order("id DESC")
+  end
+
+  def preference_params
+    params.permit(
+      :category1_id,
+      :category2_id, 
+      :genre1_id,
+      :genre2_id
+    )
   end
 end
