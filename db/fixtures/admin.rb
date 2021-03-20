fb48dc5764d158b8b6551d280b9f57d4ac4a9c54
Tatuno10 <zell8130@outlook.jp>
2021-03-20 15:05:36 +0900

    Add-profile-model-association

--- db/fixtures/admin.rb
+++ db/fixtures/admin.rb
@@ -1,2 +1,2 @@
 User.create(id: "1", name: "管理者", email: "admin@admin.jp", password: "saki8130",password_confirmation: "saki8130", admin: true)
-Preference.create(id: "1", tag1_id: "", tag2_id: "", genre1_id: "", genre2_id: "", user_id: "1")
+Preference.create(id: "1", tag1_id: "1", tag2_id: "2", genre1_id: "1", genre2_id: "2", user_id: "1")
